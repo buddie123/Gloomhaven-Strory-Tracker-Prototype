8013fcd7dc94163ed5e4d34b4a6fddbf9ec4ddbb
buddie123 <robert.reed@rocketmail.com>
2018-09-09 19:01:13 -0500

    More work on the database initialization code. Got enough data in the database to show a demo. Just need to get some more activities to add parties and attempts to the app.

--- app/src/main/java/com/atouchofjoe/ghprototye4/location/info/LocationTabFragment.java
+++ app/src/main/java/com/atouchofjoe/ghprototye4/location/info/LocationTabFragment.java
@@ -1,8 +1,9 @@
-package com.atouchofjoe.ghprototye4;
+package com.atouchofjoe.ghprototye4.location.info;
 
 import android.support.v4.app.Fragment;
 
 import com.atouchofjoe.ghprototye4.Dummy.DummyContent;
+import com.atouchofjoe.ghprototye4.MainActivity;
 import com.atouchofjoe.ghprototye4.models.Location;
 
 import java.util.List;
