c65ef95fdee1f76c4eea61e64cc3cd90c81caece
buddie123 <robert.reed@rocketmail.com>
2018-09-15 11:01:41 -0500

    Fixed #13 : Wired up a DatePicker to the dateCompleted EditText view

--- app/src/main/java/com/atouchofjoe/ghprototye4/RecordAttemptActivity.java
+++ app/src/main/java/com/atouchofjoe/ghprototye4/RecordAttemptActivity.java
@@ -1,11 +1,17 @@
 package com.atouchofjoe.ghprototye4;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class RecordAttemptActivity extends AppCompatActivity {
 
@@ -15,6 +21,29 @@ public class RecordAttemptActivity extends AppCompatActivity {
         setContentView(R.layout.activity_record_attempt);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Calendar calendar = new GregorianCalendar();
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        final int month = calendar.get(Calendar.MONTH) + 1;
+        final int year = calendar.get(Calendar.YEAR);
+
+
+        final EditText dateET = findViewById(R.id.dateCompleted);
+        dateET.setText(month + "/" + day+ "/" + year);
+        dateET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog pickerDialog = new DatePickerDialog(view.getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker,
+                                                  int year, int month, int day) {
+                                dateET.setText((month + 1) + "/" + day + "/" + year);
+                            }
+                        }, year, month - 1, day);
+                pickerDialog.show();
+            }
+        });
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
--- app/src/main/java/com/atouchofjoe/ghprototye4/location/info/LocationInfoActivity.java
+++ app/src/main/java/com/atouchofjoe/ghprototye4/location/info/LocationInfoActivity.java
@@ -85,6 +85,7 @@ public class LocationInfoActivity extends AppCompatActivity implements LoaderMan
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
